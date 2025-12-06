5003678e9772d1e26a73a5c0b15a1a2b5760c4f6
Makkes11 <m.haas1910@web.de>
2025-12-06 12:12:58 +0100

    first use of tokio async framework

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,13 +1,29 @@
 mod device;
 
 use device::{GatewayEvent, GatewayState};
+use tokio::time::Duration;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut gateway = GatewayState::new();
 
-    gateway.apply_event(GatewayEvent::Update { id: 1, value: 100 });
-    gateway.apply_event(GatewayEvent::Update { id: 2, value: 200 });
-    gateway.apply_event(GatewayEvent::Remove(1));
+    // initialize two devices
+    gateway.apply_event(GatewayEvent::Update { id: 1, value: 0 });
+    gateway.apply_event(GatewayEvent::Update { id: 2, value: 0 });
 
-    println!("{:?}", gateway);
+    // simulate device updates async
+    let mut interval = tokio::time::interval(Duration::from_secs(1));
+    for _ in 0..5 {
+        interval.tick().await;
+        // Werte ändern
+        gateway.apply_event(GatewayEvent::Update {
+            id: 1,
+            value: rand::random::<i32>() % 100,
+        });
+        gateway.apply_event(GatewayEvent::Update {
+            id: 2,
+            value: rand::random::<i32>() % 100,
+        });
+        println!("{:?}", gateway);
+    }
 }
